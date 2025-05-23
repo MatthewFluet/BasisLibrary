MatthewFluet/BasisLibrary

commit 8d5681c77a6ef8fa4690a15f4d5cba8b3657a7ea

Update mono-buffer.fun
Double the buffer size when growing it.

Code/2018/001/mono-buffer.fun
@@ -94,9 +94,11 @@ functor MonoBufferFn (
 
   (* ensure that the content array has space for amt elements *)
     fun ensureCapacity (content, len, amt) = let
+          val curCap = A.length(!content)
+          val amt = Int.max(curCap - len + curCap, amt)
           val capacity = (len + amt) handle Overflow => maxLen
           in
-            if (A.length(!content) < capacity)
+            if (curCap < capacity)
               then let
                 val newArr = A.array(capacity, defaultElem)
                 in

commit 9488e6b111503d25729b27bed2da6f8c3a240edc

Fix bug in mono-buffer.fun introduced by 8d5681c
The intention of 8d5681c was to double the buffer size when
`ensureCapacity` must grow the buffer, but mistakenly doubled the
buffer size every time `ensureCapacity` was called, leading to an
uncaught `Overflow` exception after sufficient operations that call
`ensureCapacity` before adding content to the buffer.

See MLton/mlton#608.

Code/2018/001/mono-buffer.fun
@@ -95,12 +95,12 @@ functor MonoBufferFn (
   (* ensure that the content array has space for amt elements *)
     fun ensureCapacity (content, len, amt) = let
           val curCap = A.length(!content)
-          val amt = Int.max(curCap - len + curCap, amt)
-          val capacity = (len + amt) handle Overflow => maxLen
+          val ensCap = (len + amt) handle Overflow => maxLen
           in
-            if (curCap < capacity)
+            if (curCap < ensCap)
               then let
-                val newArr = A.array(capacity, defaultElem)
+                val newCap = Int.max(2 * curCap handle Overflow => maxLen, ensCap)
+                val newArr = A.array(newCap, defaultElem)
                 in
                   AS.copy{dst = newArr, di = 0, src = AS.slice(!content, 0, SOME len)};
                   content := newArr